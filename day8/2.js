3a09928444945d1b39ee1c3aad161addd749b527
Sergey Vasilkov <sergey.vasilkov@snyk.io>
2020-12-08 23:46:08 +0200

    day 8: parts one and two

---
+++ day8/2.js
@@ -0,0 +1,59 @@
+const lib = require("../lib");
+
+const commandsList = lib.readFileToArray("./input.txt").filter(r => r !== '');
+
+function runBootCode(commandsList, startFrom = 0, startAccum = 0, mutate = true) {
+    const steps = []
+    let accum = startAccum;
+    let currentIndex = startFrom;
+    const lastIndex = commandsList.length-1;
+
+    const executor = {
+        acc(param) {
+            accum += param;
+            currentIndex += 1;
+        },
+        jmp(param) {
+            currentIndex += param;
+        },
+        nop() {
+            currentIndex += 1;
+        }
+    }
+
+    while(currentIndex <= lastIndex || !steps.includes(currentIndex)) {
+        steps.push(currentIndex)
+
+        let [command, param] = commandsList[currentIndex].split(' ');
+        
+        if (mutate && command === 'nop') {
+            const modifiedCommandsList = [...commandsList]
+            modifiedCommandsList[currentIndex] = `jmp ${param}`;
+            if (runBootCode(modifiedCommandsList, currentIndex, accum, false)) {
+                break;
+            }
+        }
+
+        if (mutate && command === 'jmp') {
+            const modifiedCommandsList = [...commandsList]
+            modifiedCommandsList[currentIndex] = `nop ${param}`;
+
+            if (runBootCode(modifiedCommandsList, currentIndex, accum, false)) {
+                break;
+            }
+        }
+
+        executor[command](parseInt(param))
+
+        if (currentIndex > lastIndex) {
+            console.log(`TERMINATING WITH ACCUM=${accum}`)
+            return true;
+        }
+
+        if (steps.includes(currentIndex)) {
+            return false;
+        }
+    }
+}
+
+runBootCode(commandsList)
